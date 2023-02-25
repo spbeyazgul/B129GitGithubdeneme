915feefc9366447cce0e98725083bbc567279a61
spbeyazgul <stbeyazgul@gmail.com>
2023-02-25 18:44:45 +0300

    v2

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,6 +1,9 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Dünya");
 
     }
 }
+//Restore --> Tamamen Work,ng Space'i temizler
+//
